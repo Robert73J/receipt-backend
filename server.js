35c8efb35773ba8d5640a0aab34072920bfa7703
Robert Njunji <robertnjunji@gmail.com>
2026-06-11 22:08:47 +0300

    Deployment verification

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const pool = require("./db");
+console.log("=== DEPLOY VERSION TEST ===");
+console.log("Debug build deployed");
 
 (async () => {
   try {
